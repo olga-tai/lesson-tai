5077cfe5b2063c236b0d20c4a09af642d5fb2b09
olga-tai <oly.droly@gmail.com>
2024-10-30 01:37:16 +0800

    Add files via upload
    Первая программа с арифметическими вычислениями

---
+++ arifmetika.py
@@ -0,0 +1,8 @@
+print(9 ** 0.5 * 5)                         # 1st program
+print(9.99 > 9.98) and  (1000 != 1000.1)    # 2nd program
+print(2*2+2)                                # 3rd program"
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+print('123.456') == type(float(123.456))    # 4th program
+print (123.456*10)
+print(int(float('123.456') * 10) % 10)
